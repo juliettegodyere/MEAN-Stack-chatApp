b641049a4e029124769682cde987c8ceca0e1b61
juliettegodyere <juliettejuli@yahoo.com>
2016-06-03 17:13:57 +0100

    initial

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+io.on('connection', function(socket){
+  //socket.on('adduser', function(username){
+  //  socket.username = username;
+  //  socket.emit('updatedate', SERVER,'you are connected');
+  //}),
+
+   console.log('a user connected');
+  /*
+  { username : danet, message : "Hi everybody"}
+  */
+  socket.on('chat_message', function(msg){
+    console.log('New message: ' + msg);
+    io.emit('new_message', msg);
+  });
+  
+  // socket.on('blabla', function(msg){
+  //   console.log('User said '+msg);
+  //   io.emit('message', msg);
+  // })
+
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
