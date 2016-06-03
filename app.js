26aa8f62dcafbe7caf76fd3255e36b466dd2bac8
contactdanny19@gmail.com <contactdanny19@gmail.com>
2016-06-03 17:27:02 +0100

    change port number

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(1337, function(){
+  console.log('listening on *:1337');
 });
 
